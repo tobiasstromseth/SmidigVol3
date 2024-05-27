1c449facfa6a64cd75bdc98f40d7495d9a0f0a5f
Tobias Strømseth <ts16.12.2000@gmail.com>
2024-05-27 15:47:27 +0200

    Initial commit

--- code/test.py
+++ code/test.py
@@ -1,4 +1,5 @@
 import webview
 
-window = webview.create_window('Woah dude!', 'https://pywebview.flowrl.com')
+first_window = webview.create_window('Woah dude!', 'https://pywebview.flowrl.com')
+second_window = webview.create_window('Second window', 'https://woot.fi')
 webview.start()
